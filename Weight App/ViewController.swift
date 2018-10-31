80021665ea9383f7fe473aa18041e822b69cdd43
period2 <period2@p-162mba-06.d214.org>
2018-10-31 09:46:01 -0500

    megapush

--- Weight App/ViewController.swift
+++ Weight App/ViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 
 }
