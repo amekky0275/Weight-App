0f9633772d8a961751a481fb101a716ba657d0e6
amekky0275 <amekky0275@stu.d214.org>
2018-11-16 15:16:55 +0000

    removed tableViewCell file because its useless

---
+++ Weight App/LiftViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LiftViewController.swift
+//  Weight App
+//
+//  Created by period2 on 11/15/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class LiftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
